3e0a658cf3542ecec3f09efb4cd83cb9554c63d8
Jesse Stuart <jesse@jessestuart.ca>
2014-06-09 19:25:40 -0400

    Make it so

---
+++ README.md
@@ -0,0 +1,52 @@
+ScrollPagination
+================
+
+[React](http://reactjs.org/) component for handing infinite scroll without infinitely growing the DOM.
+
+## Usage
+
+```
+<ScrollPagination
+	pageIds={ Array() }
+	hasPrevPage={ Boolean() }
+	hasNextPage={ Boolean() }
+	loadPrevPage={ function(){} }
+	loadNextPage={ function(){} }
+	unloadPage={ function (pageId) {} }>
+
+	<h1>Render content here</h1>
+</ScrollPagination>
+```
+
+- `pageIds` must be an array of all page ids currently rendered. The initial render must only contain a single page id and subsequent renders must only add or remove a single page id.
+
+- `hasPrevPage` must be a boolean indicating if `loadPrevPage` should be called.
+
+- `hasNextPage` must be a boolean indicating if `loadNextPage` should be called.
+
+- `loadPrevPage` must be a function that causes a re-render with a single page prepended.
+
+- `loadNextPage` must be a function that causes a re-render with a single page appended.
+
+- `unloadPage` must be a function that causes a re-render with the specified page id removed.
+
+## What it does
+
+The scroll container will be determined when the component is first mounted (any parent element with `overflow:auto` or `overflow:scroll`, defaults to `window`).
+
+`loadPrevPage` and `loadNextPage` are called when there is one thrid or less of the current page height out of view above and below respectively.
+
+`unloadPage` is called before `loadPrevPage` or `loadNextPage` if there is a page at the opposite end of the visible area entirely out of view.
+
+The height of each rendered page is calculated and cached to determine when a new page should be loaded or an old one removed. Page heights for pages removed from the top are also kept to help keep the padding accurate if/when the page is loaded again.
+
+## Why
+
+Infinite scroll implementations where DOM nodes are continually added without cleaning up cause the DOM to get __very__ slow the farther you scroll down (especially when you have any kind of dynamic content). Removing what you don't need from the DOM and replacing it with padding allows scrolling through pages without slowing down the DOM.
+
+## How you can help
+
+File an [issue](https://github.com/cupcake/react-scroll-pagination/issues) if you find anything isn't working as expected.
+
+Pull requests are always welcome, but you should open an issue before working on a new feature (both to ensure it's within the scope of this project and that it's not already being worked on).
+
---
+++ ScrollPagination.js
@@ -0,0 +1,286 @@
+/** @jsx React.DOM */
+
+(function () {
+"use strict";
+
+var findScrollParent = function (el) {
+	var ref = el;
+	while (ref) {
+		switch (window.getComputedStyle(ref).overflow) {
+			case "auto":
+				return ref;
+			case "scroll":
+				return ref;
+		}
+		ref = ref.parentElement;
+	}
+	return window;
+};
+
+window.ScrollPagination = React.createClass({
+	displayName: "ScrollPagination",
+
+	getDefaultProps: function () {
+		return {
+			pageIds: [],
+			loadPrevPage: function () {},
+			loadNextPage: function () {}
+		};
+	},
+
+	componentWillMount: function () {
+		this.__paddingTop = 0;
+		this.__offsetHeight = 0;
+		this.__offsetTop = 0;
+		this.__offsetBottom = 0;
+
+		this.__renderedPageIds = [];
+		this.__unloadedPageHeights = {};
+		this.__pageHeights = {};
+	},
+
+	componentDidMount: function () {
+		var scrollParent = this.__scrollParent = findScrollParent(this.getDOMNode());
+		this.__updateDimensions();
+		this.__evaluatePagesMutation();
+		scrollParent.addEventListener("scroll", this.__handleScroll, false);
+		scrollParent.addEventListener("resize", this.__handleResize, false);
+
+		this.__hasPrevPage = this.props.hasPrevPage;
+		this.__hasNextPage = this.props.hasNextPage;
+	},
+
+	componentWillUpdate: function (props) {
+		this.__loadingPrevPage = false;
+		this.__loadingNextPage = false;
+		this.__unloadingPage = false;
+
+		// save scroll position
+		this.__scrollY = this.__scrollParent.scrollTop;
+
+		this.__determinePagesDelta(props.pageIds);
+
+		this.__hasPrevPage = props.hasPrevPage;
+		this.__hasNextPage = props.hasNextPage;
+	},
+
+	componentDidUpdate: function () {
+		this.__updateDimensions();
+
+		// restore scroll position
+		this.__scrollParent.scrollTop = this.__scrollY;
+		delete this.__scrollY;
+
+		this.__evaluatePagesMutation();
+	},
+
+	componentWillUnmount: function () {
+		var scrollParent = this.__scrollParent;
+		scrollParent.removeEventListener("scroll", this.__handleScroll, false);
+		scrollParent.removeEventListener("resize", this.__handleResize, false);
+	},
+
+	render: function () {
+		var style = {};
+		if (this.__paddingTop) {
+			style.paddingTop = this.__paddingTop + "px";
+		}
+		return (
+			<div ref="wrapper" style={style}>
+				{this.props.children}
+			</div>
+		);
+	},
+
+	__unloadPage: function (pageId) {
+		if (this.__unloadingPage) {
+			return;
+		}
+		this.__unloadingPage = true;
+		this.props.unloadPage(pageId);
+	},
+
+	__loadPrevPage: function () {
+		if (this.__loadingPrevPage || !this.__hasPrevPage) {
+			return;
+		}
+		this.__loadingPrevPage = true;
+		this.props.loadPrevPage();
+	},
+
+	__loadNextPage: function () {
+		if (this.__loadingNextPage || !this.__hasNextPage) {
+			return;
+		}
+		this.__loadingNextPage = true;
+		this.props.loadNextPage();
+	},
+
+	__determinePagesDelta: function (pageIds) {
+		var renderedPageIds = this.__renderedPageIds;
+
+		// find new page id (if any)
+		// must be either at beginning or end of list
+		var newPageId = null;
+		var newPagePosition = null;
+		var unloadedPageId = null;
+		var unloadedPagePosition = null;
+		var firstPageId = pageIds[0];
+		var lastPageId = pageIds[pageIds.length-1];
+		if (renderedPageIds.indexOf(lastPageId) === -1) {
+			newPageId = lastPageId;
+			newPagePosition = "bottom";
+		} else if (renderedPageIds.indexOf(firstPageId) === -1) {
+			newPageId = firstPageId;
+			newPagePosition = "top";
+		} else if (pageIds.length > renderedPageIds.length) {
+			throw new Error("ScrollPagination: New pages must be inserted at beginning or end!\nold("+ renderedPageIds.join(", ") +")\nnew("+ pageIds.join(", ") +")");
+		} else {
+			// find removed page id
+			// must be either at beginning or end of list
+			var firstRenderedPageId = renderedPageIds[0];
+			var lastRenderedPageId = renderedPageIds[renderedPageIds.length-1];
+			if (firstRenderedPageId !== firstPageId) {
+				unloadedPageId = renderedPageIds[0];
+				unloadedPagePosition = "top";
+			} else if (lastRenderedPageId !== lastPageId) {
+				unloadedPageId = lastRenderedPageId;
+				unloadedPagePosition = "bottom";
+			}
+		}
+
+		var pageNumDelta = pageIds.length - renderedPageIds.length;
+		if (newPageId && pageNumDelta !== 1 || unloadedPageId && pageNumDelta !== -1) {
+			throw new Error("ScrollPagination: May only add or remove a single page but there is a difference of "+ pageNumDelta +"!");
+		}
+
+		renderedPageIds = [].concat(pageIds);
+		this.__renderedPageIds = renderedPageIds;
+
+		this.__newPageId = newPageId;
+		this.__newPagePosition = newPagePosition;
+		this.__unloadedPageId = unloadedPageId;
+		this.__unloadedPagePosition = unloadedPagePosition;
+	},
+
+	__updateDimensions: function () {
+		var unloadedPageHeights = this.__unloadedPageHeights;
+		var pageHeights = this.__pageHeights;
+
+		var newPageId = this.__newPageId;
+		var newPagePosition = this.__newPagePosition;
+		delete this.__newPagePosition;
+
+		var unloadedPageId = this.__unloadedPageId;
+		var unloadedPagePosition = this.__unloadedPagePosition;
+		delete this.__unloadedPageId;
+		delete this.__unloadedPagePosition;
+
+		if (unloadedPageId) {
+			delete pageHeights[unloadedPageId];
+		}
+
+		var el = this.refs.wrapper.getDOMNode();
+
+		var oldOffsetHeight = this.__offsetHeight;
+		var newOffsetHeight = el.offsetHeight;
+		var offsetHeightDelta = newOffsetHeight - oldOffsetHeight;
+		var oldPageHeight;
+		var pageHeightDelta = 0;
+
+		var scrollParent = this.__scrollParent;
+
+		if (newPageId) {
+			if (newPagePosition === "top") {
+				var oldPaddingTop = this.__paddingTop;
+				oldPageHeight = unloadedPageHeights[newPageId];
+				delete unloadedPageHeights[newPageId];
+				if (oldPageHeight && oldPageHeight !== offsetHeightDelta) {
+					pageHeightDelta = oldPageHeight - offsetHeightDelta;
+				}
+				if (this.__paddingTop < 1) {
+					var __scrollTop = scrollParent.scrollTop;
+					this.__scrollY = __scrollTop + offsetHeightDelta;
+				}
+				this.__paddingTop = Math.max(this.__paddingTop - offsetHeightDelta - pageHeightDelta, 0);
+				this.refs.wrapper.getDOMNode().style.paddingTop = this.__paddingTop +"px";
+			} else { // bottom
+			}
+
+			pageHeights[newPageId] = offsetHeightDelta;
+		} else {
+			if (unloadedPagePosition === "top") {
+				oldPaddingTop = this.__paddingTop;
+				this.__paddingTop = this.__paddingTop + (offsetHeightDelta * -1); // negative delta
+				unloadedPageHeights[unloadedPageId] = offsetHeightDelta * -1;
+				this.refs.wrapper.getDOMNode().style.paddingTop = this.__paddingTop +"px";
+			}
+		}
+
+		newOffsetHeight = el.offsetHeight;
+		this.__offsetHeight = newOffsetHeight;
+
+		var offsetTop = 0;
+		var ref = el;
+		while (ref) {
+			offsetTop += ref.offsetTop || 0;
+			ref = ref.offsetParent;
+		}
+		this.__offsetTop = offsetTop;
+
+		if (scrollParent) {
+			this.__viewportHeight = parseInt(window.getComputedStyle(scrollParent).height, 10);
+		}
+	},
+
+	__evaluatePagesMutation: function (e) {
+		if (this.__offsetHeight === 0) {
+			return;
+		}
+		if (this.__loadingNextPage || this.__loadingPrevPage || this.__unloadingPage) {
+			if (e) {
+				e.preventDefault();
+			}
+			return;
+		}
+
+		var scrollY = this.__scrollParent.scrollTop;
+		var viewportHeight = this.__viewportHeight;
+		var paddingTop = this.__paddingTop;
+		var remainingScrollBottom = this.__offsetHeight + this.__offsetBottom - scrollY - viewportHeight;
+		var pagesOffsetTop = this.__offsetTop + paddingTop;
+		var remainingScrollTop = scrollY - pagesOffsetTop;
+
+		var pageIds = this.props.pageIds;
+		var pageHeights = this.__pageHeights;
+		var firstPageId = pageIds[0];
+		var firstPageHeight = pageHeights[firstPageId];
+		var lastPageId = pageIds[pageIds.length-1];
+		var lastPageHeight = pageHeights[lastPageId];
+
+		if ( !lastPageHeight || remainingScrollBottom <= (lastPageHeight / 3)) {
+			if (firstPageHeight && (scrollY - pagesOffsetTop - firstPageHeight) > viewportHeight && this.__newPageId !== firstPageId) {
+				this.__unloadPage(firstPageId);
+			} else {
+				this.__loadNextPage();
+			}
+		} else if ((paddingTop > 1 || this.__hasPrevPage) && remainingScrollTop <= (firstPageHeight / 3)) {
+			if (pageIds.length > 1 && (this.__offsetHeight - scrollY - viewportHeight) > lastPageHeight && this.__newPageId !== lastPageId) {
+				this.__unloadPage(lastPageId);
+			} else {
+				this.__loadPrevPage();
+			}
+		}
+	},
+
+	__handleScroll: function (e) {
+		this.__evaluatePagesMutation(e);
+	},
+
+	__handleResize: function () {
+		this.__updateDimensions();
+		this.__evaluatePagesMutation();
+	}
+});
+
+})();
